02ff1341b710a4ee8ea4c22119697beab3b36587
uttamsaha <uttamsaha982@gmail.com>
2022-07-05 09:46:23 +0600

    input validation added

--- js/banking.js
+++ js/banking.js
@@ -1,31 +1,43 @@
-document.getElementById('deposit-btn').addEventListener('click', function(){
-    const depositInput = document.getElementById('deposit-input');
-    const depositText = document.getElementById('deposit-text');
-    const currentDepositText = depositText.innerText;
-      const depositInputValue = depositInput.value;
-    depositText.innerText = parseFloat(currentDepositText) + parseFloat(depositInputValue);
-    depositInput.value = '';
+document.getElementById("deposit-btn").addEventListener("click", function () {
+  const depositInput = document.getElementById("deposit-input");
+  if(depositInput === '' || isNaN(depositInput)){
+    alert('Input Filed Empty or Enter valid number.');
+    return;
+  }
+  const depositText = document.getElementById("deposit-text");
+  const currentDepositText = depositText.innerText;
+  const depositInputValue = depositInput.value;
+  depositText.innerText =
+    parseFloat(currentDepositText) + parseFloat(depositInputValue);
+  depositInput.value = "";
 
-    const balanceText = document.getElementById('balance-text');
-    const updatedBalance = parseFloat(depositInputValue) + parseFloat(balanceText.innerText);
-    balanceText.innerText = updatedBalance;
-
+  const balanceText = document.getElementById("balance-text");
+  const updatedBalance =
+    parseFloat(depositInputValue) + parseFloat(balanceText.innerText);
+  balanceText.innerText = updatedBalance;
 });
 
-document.getElementById('withdraw-btn').addEventListener('click', function(){
-    const withdrawInput = document.getElementById('withdraw-input');
-    const currentWithdrawInput = withdrawInput.value;
-    
-    const withdrawText = document.getElementById('withdraw-text');
-    const currentWithdrawText = withdrawText.innerText;
+document.getElementById("withdraw-btn").addEventListener("click", function () {
+  const withdrawInput = document.getElementById("withdraw-input");
+  const currentWithdrawInput = withdrawInput.value;
+  if(currentWithdrawInput === '' || isNaN(currentWithdrawInput)){
+    alert('Input Filed Empty or Enter valid number.');
+    return;
+  }
 
-    const updatedWithdrawText = parseFloat(currentWithdrawInput) + parseFloat(currentWithdrawText);
-    withdrawText.innerText = updatedWithdrawText;
-    withdrawInput.value = '';
+  const withdrawText = document.getElementById("withdraw-text");
+  const currentWithdrawText = withdrawText.innerText;
+  
 
-    const balance = document.getElementById('balance-text');
-    const currentBalance = balance.innerText;
+  const updatedWithdrawText =
+    parseFloat(currentWithdrawInput) + parseFloat(currentWithdrawText);
+  withdrawText.innerText = updatedWithdrawText;
+  withdrawInput.value = "";
 
-    const updatedBalance = parseFloat(currentBalance) - parseFloat(currentWithdrawInput);
-    balance.innerText = updatedBalance;
-})
+  const balance = document.getElementById("balance-text");
+  const currentBalance = balance.innerText;
+
+  const updatedBalance =
+    parseFloat(currentBalance) - parseFloat(currentWithdrawInput);
+  balance.innerText = updatedBalance;
+});
--- js/login.js
+++ js/login.js
@@ -1,14 +1,13 @@
-    alert("Please login username: admin password: admin");
-document.getElementById('login-btn').addEventListener('click', function(){
-    const userName = document.getElementById('user-name');
-    const username = userName.value;
-  
-    const pass = document.getElementById('password');
-    const password = pass.value;
-    if(username=='admin' && password=='admin'){
-        window.location.href = 'banking.html';
-    }
-    else{
-        alert('Wrong username or password Enter username:admin password: admin');
-    }
-})
+alert("Please login username: admin password: admin");
+document.getElementById("login-btn").addEventListener("click", function () {
+  const userName = document.getElementById("user-name");
+  const username = userName.value;
+
+  const pass = document.getElementById("password");
+  const password = pass.value;
+  if (username == "admin" && password == "admin") {
+    window.location.href = "banking.html";
+  } else {
+    alert("Wrong username or password Enter username:admin password: admin");
+  }
+});
